ffc9f297c1d06aaedc2db4750bf44a25a65db9ac
manjot kaur <105725308@student.swin.edu.au>
2025-05-25 23:48:19 +1000

    Committed remaining local changes before pulling

--- GO1-COS5_part2/manage.php
+++ GO1-COS5_part2/manage.php
@@ -1,20 +1,77 @@
 <?php
 require_once("settings.php");
 
-$query = "SELECT * FROM eoi ORDER BY EOInumber DESC";
-$result = mysqli_query($conn, $query);
+function displayResults($result) {      //this is a table created to retrieve data from  my sql query 
+    // and for the manager to search different parts of it .
+    if (mysqli_num_rows($result) > 0) {
+echo "<table border='1'><tr>
+<th>EOInumber</th><th>Job ref</th><th>Name</th><th>Email</th>
+<th>Phone</th><th>status</th></tr>";
+while ($row = mysqli_fetch_assoc($result)){
 
-if ($result && mysqli_num_rows($result) > 0) {
-    echo "<table border='1' cellpadding='5'>";
-    echo "<tr><th>EOInumber</th><th>Job Ref No</th><th>First Name</th>...etc</tr>";
-    while ($row = mysqli_fetch_assoc($result)) {
-        // output rows
+echo "<tr>
+<td>{$row['EOInumber']}</td>
+<td>{$row['JobRefNumber']}</td>
+<td>{$row['FirstName']}</td>
+<td>{$row['LastName']}</td>
+<td>{$row['Email']}</td>
+<td>{$row['Phone']}</td>
+<td>{$row['Status']}</td>
+</tr>";
+}                echo "</table><br>";
+            }else {
+        echo "<p> No Result found.</p>" ;
     }
-    echo "</table>";
-} else {
-    echo "<p>No EOI records found.</p>";
 }
 
+// List of all Eois mentioned
+
+if (!isset($_POST['search_job']) && !isset($_POST['search_name']) && !isset($_POST['delete_eoi']) && !isset($_POST['update_status'])) {
+    $result = mysqli_query($conn, "SELECT * FROM eoi");
+    echo "<h2>All EOIs</h2>";
+    displayResults($result);
+}
+
+// Search by Job Reference
+if (isset($_POST['search_job'])) {
+    $job_ref = mysqli_real_escape_string($conn, $_POST['job_ref']);
+    $result = mysqli_query($conn, "SELECT * FROM eoi WHERE JobRefNumber = '$job_ref'");
+    echo "<h2> EOIs for Job Ref: $job_ref</h2>";
+    displayResults($result);
+}
+// Search by Name
+if (isset($_POST['search_name'])) {
+    $fname = mysqli_real_escape_string($conn, $_POST['first_name']);
+    $lname = mysqli_real_escape_string($conn, $_POST['last_name']);
+    $sql = "SELECT * FROM eoi WHERE FirstName LIKE '%$fname%' OR LastName LIKE '%$lname%'";
+    $result = mysqli_query($conn, $sql);
+    echo "<h2>EOIs for Applicant: $fname $lname</h2>";
+    displayResults($result);
+}
+
+// Delete EOIs by Job Reference
+if (isset($_POST['delete_eoi'])) {
+    $job_ref = mysqli_real_escape_string($conn, $_POST['job_ref_delete']);
+    $sql = "DELETE FROM eoi WHERE JobRefNumber = '$job_ref'";
+    if (mysqli_query($conn, $sql)) {
+        echo "<p>Deleted all EOIs for Job Ref: $job_ref</p>";
+    } else {
+        echo "<p>Error deleting EOIs. </p>";
+    }
+}
+
+
+// Changing the status of jobs
+if (isset($_POST['update_status'])) {
+    $eoi = (int)$_POST['eoi_number'];
+    $new_status = mysqli_real_escape_string($conn, $_POST['new_status']);
+    $sql = "UPDATE eoi SET Status = '$new_status' WHERE EOInumber = $eoi";
+    if (mysqli_query($conn, $sql)) {
+        echo "<p>Updated status of EOI #$eoi to '$new_status'.</p>";
+    } else {
+        echo "<p>Error updating status.</p>";
+    }
+}
 mysqli_close($conn);
 
 ?>
@@ -46,7 +103,7 @@ mysqli_close($conn);
 <!-- Update Status -->
 <form method="post">
     <h3>Update EOI Status</h3>
-    <input type="number" name="eoi_number" placeholder="EOI #" required>
+    <input type="number" name="eoi_number"  placeholder="EOI #" required>
     <select name="new_status">
         <option value="New">New</option>
     <option value="Current">Current</option>

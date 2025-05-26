9a01a09de9d91bbf41c5d1d0ac33a9bd8282ad5d
JoseHTy <105927997@student.swin.edu.au>
2025-05-26 21:42:18 +1000

    Put all files into "project2" folder, tweaked some files for css

--- GO1-COS5_part2/project2/login.php
+++ GO1-COS5_part2/project2/login.php
@@ -1,5 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>About Us</title>
+  <meta name="description" content="About Us" />
+  <meta name="keywords" content="software, web, technology" />
+  <meta name="author" content="Jacob Semmel" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="styles/styles.css" />
+</head>
+<body>
+
+
+
 <?php
 session_start();
+    include 'nav.inc'; 
+
+    include 'header.inc'; 
+
 
 $username = 'admin';
 $password = 'password123';
@@ -22,3 +41,8 @@ if (isset($_POST['login'])) {
 </form>
 
 <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+
+
+
+</body>
+</html>
--- GO1-COS5_part2/project2/manage.php
+++ GO1-COS5_part2/project2/manage.php
@@ -1,4 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>About Us</title>
+  <meta name="description" content="About Us" />
+  <meta name="keywords" content="software, web, technology" />
+  <meta name="author" content="Manjot" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="styles/styles.css" />
+</head>
+<body>
+
+
+
+
 <?php
+     include 'nav.inc'; 
+
+     include 'header.inc'; 
+
+
 require_once("settings.php");
 
 function displayResults($result) {      
@@ -111,3 +132,5 @@ mysqli_close($conn);
 </form>
 
 
+</body>
+</html>

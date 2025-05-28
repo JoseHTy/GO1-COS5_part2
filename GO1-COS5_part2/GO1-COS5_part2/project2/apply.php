6a0bd0616ffb3aed1e5e56dbcffb37370791432d
JoseHTy <105927997@student.swin.edu.au>
2025-05-28 18:56:45 +1000

    Fixed Process_eoi

---
+++ GO1-COS5_part2/GO1-COS5_part2/project2/apply.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Job Application Page" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Jose Ty" />
+    <title>Job Application</title>
+    <link rel="stylesheet" href="styles/styles.css" />
+</head>
+
+
+
+<body>
+
+     <?php include 'nav.inc'; ?>
+
+     <?php include 'header.inc'; ?>
+
+   
+
+    <?php include 'apply.inc'; ?>
+
+
+     <?php include 'footer.inc'; ?>
+  
+</body>
+</html>
+
